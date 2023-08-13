3bb61ad6178f5674f5878f5d27cf69d006dfd7c7
Yaşar Yiğit Turan <125077156+yyigitturan@users.noreply.github.com>
2023-08-13 20:34:57 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Animasyonlu sayfa geçişleri için smooth scroll işlevi
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
